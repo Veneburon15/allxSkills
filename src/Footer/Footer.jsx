41ba8fdc5fd68794860b583f3281b214207c2afa
Angel Leon <angel.leon.ram@gmail.com>
2023-11-09 14:34:25 -0300

    Redimensionamientos del footer y el navbar

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -4,44 +4,43 @@ import './Footer.css'
 const Footer = () => {
     return (
         <footer className='footerContainer'>
-                <section className='secciones'>
-                    <section className='footerSection1'>
-                        <img src={Logo} alt="AllxSkillz Logo" className='footerLogo'/>
-                        <p className='footerPartnerP text'>Tu Socio en el Desarrollo <br/> de Habilidades Blandas</p> 
-                        <p className='phoneNumber text'>(+54) 911 3921-4786</p>
-                        <ul>
-                            <li><img src="#" alt="WhatsApp Icon"/></li>
-                            <li><img src="#" alt="Email Icon"/></li>
-                            <li><img src="#" alt="LinkedIn Icon"/></li>
-                            <li><img src="#" alt="Threads Icon"/></li>
-                            <li><img src="#" alt="Instagram Icon"/></li>
-                        </ul>
-                    </section>
-                    <section className='footerSection2'>
-                        <h3 className='anchorTitle'>Enlaces de interés</h3>
-                        <ul>
-                            <li><a href="#" className='text'>Oferta Académica</a></li>
-                            <li><a href='#' className='text'>Sobre Nosotros</a></li>
-                            <li><a href='#' className='text'>¿Por qué Soft Skills?</a></li>
-                            <li><a href='#' className='text'>Contacto</a></li>
-                         </ul>
-                    </section>
-                    <section className='footerSection3'>
-                        <div className='topRightSection'>
-                            <h2 className='footerh2'>Suscríbete para novedades</h2>
-                            <div className='emailInput'>
-                                <input className="text" placeholder='Email'/>
-                                <button>{">"}</button>
-                            </div>
-                        </div>
-                    </section>
+            <section className='secciones'>
+                <section className='footerSection1'>
+                    <img src={Logo} alt="AllxSkillz Logo" className='footerLogo'/>
+                    <p className='footerPartnerP text'>Tu Socio en el Desarrollo <br/> de Habilidades Blandas</p> 
+                    <p className='phoneNumber text'>(+54) 911 3921-4786</p>
+                    <ul>
+                        <li><img src="#" alt="WhatsApp Icon"/></li>
+                        <li><img src="#" alt="Email Icon"/></li>
+                        <li><img src="#" alt="LinkedIn Icon"/></li>
+                        <li><img src="#" alt="Threads Icon"/></li>
+                        <li><img src="#" alt="Instagram Icon"/></li>
+                    </ul>
                 </section>
+                <section className='footerSection2'>
+                    <ul>
+                        <li><a href="#" className='text'>Oferta Académica</a></li>
+                        <li><a href='#' className='text'>Sobre Nosotros</a></li>
+                        <li><a href='#' className='text'>¿Por qué Soft Skills?</a></li>
+                        <li><a href='#' className='text'>Contacto</a></li>
+                    </ul>
+                </section>
+                <section className='footerSection3'>
+                        <h2 className='footerh2'>Suscríbete para novedades</h2>
+                        <form className='emailInput'>
+                            <label for="email" className='emailLabel'>
+                                <input className="text" placeholder='Correo' id='emailFooter'/>
+                            </label>
+                            <input type="submit" name='submit' value={">"} id='submitFooter'/>
+                        </form>
+                </section>
+            </section>
             <section className='copyrights'>
                 <p className='text'>Copyright © 2023 Allskills</p>
-                <div className='belowRightSection'>
-                    <a className='text'>Términos y Condiciones</a>
-                    <a className='text'>Privacidad</a>
-                </div>
+                <ul className='belowRightSection'>
+                    <li><a href='#' className='text'>Términos y Condiciones</a></li>
+                    <li><a href='#' className='text'>Privacidad</a></li>
+                </ul>
             </section>
         </footer>
     )
