10b9a3e1ecdb6ac5ba6e2f9f6b7fe0a3b8f9af57
LopezVillaFrancisco <franlopezvilla8@gmail.com>
2023-11-17 12:26:44 -0300

    Pagina de contacto y cambios en pagina de oferta academica

--- src/Oferta Academica/OfertaAcademica.jsx
+++ src/Oferta Academica/OfertaAcademica.jsx
@@ -1,7 +1,6 @@
 import HeadOferta from './HeadOferta/HeadOferta'
 import HeroOferta from './HeroOferta/HeroOferta' 
 import Form from "../Home/Form/Form"
-import Preguntas from "./Preguntas/Preguntas" 
 import NuestrosCursos from "./NuestrosCursos/NuestrosCursos"
 import './OfertaAcademica.css'
 
