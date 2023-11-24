bcc82e2b94c7a0c871c4d589323fe69658d83d73
Ángel León <angel.leon.ram@gmail.com>
2023-11-24 16:37:06 -0300

    Update MainHero.jsx

--- src/SoftSkillsPage/MainHero/MainHero.jsx
+++ src/SoftSkillsPage/MainHero/MainHero.jsx
@@ -1,9 +1,9 @@
 import './MainHero.css'
 const MainHero = () => {
     return(
-        <section>
-            <div>
-                <h2>Desarrolla <span>Soft Skills</span>, alcanza tus metas</h2>
+        <section className='mainSectionHero'>
+            <div className='textDiv'>
+                <h2>Desarrolla <p className='heroTextGradient'>Soft Skills</p>, alcanza tus metas</h2>
                 <p>Construye un perfil sólido y prepárate para destacar en cualquier entorno laboral</p>
             </div>
         </section>
